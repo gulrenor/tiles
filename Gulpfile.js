d74f43eabcf24c742d6ffd638c1ff1fddfcec943
Tim <gulrenor@gmail.com>
2017-05-30 11:55:39 -0400

    Changed autoprefixer settings

--- Gulpfile.js
+++ Gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('styles', function() {
   gulp.src(dirSass_In)
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer('last 2 versions', 'safari >=8', 'ie >= 10', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(dirCSS_Out))
     .pipe(rename({
       suffix: '.min'
